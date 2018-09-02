3ddd5dd82406bff508abe032f5a9f0375e9cc56a
Tal Baneth <tal@kyber.network>
2018-09-02 20:01:49 +0300

    Duplicate zeppelin tests logic to test the WBTC token.
    As in zeppelin tests, the different test behaviors are separated to different
    files.
    
    In order to examine differences with zeppelin tests one can diff the behaviour
    files with those of zeppelin (under node_modules/open_zeppelin/tests/..).
    
    Importing (with require) the actual zeppelin tests is impossible since some of
    their tests are without separation to behavior and test token.
    Also as some of the token logic is overriden in wbtc, some of their test
    logic should be changed anyway.
    
    Also added mock contracts used for these tests, as zeppelin npm does not
    include the mocks and their testing scripts.

---
+++ contracts/mock/BasicTokenMock.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.24;
+
+import "openzeppelin-solidity/contracts/token/ERC20/BasicToken.sol";
+
+
+// mock class using BasicToken
+contract BasicTokenMock is BasicToken {
+
+  constructor(address _initialAccount, uint256 _initialBalance) public {
+    balances[_initialAccount] = _initialBalance;
+    totalSupply_ = _initialBalance;
+  }
+
+}
